e49dba896d27563934442c5d84c9d394cf948a36
mariosn-phys <132878795+mariosn-phys@users.noreply.github.com>
2024-03-31 17:57:04 +0300

    Update init_path.m

--- DNS_Library_v1/init_path.m
+++ DNS_Library_v1/init_path.m
@@ -1,4 +1,4 @@
-init_structure = 1; %% Generates the noise basis for a 61x72x72 grid
+init_structure = 1; %% Generates the noise basis for a 61x72x72 grid, set to 0 after 1st run
 addpath(genpath('../'))
 
 if init_structure
