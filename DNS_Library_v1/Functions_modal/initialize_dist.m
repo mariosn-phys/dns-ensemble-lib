c6315574c033c9a54964bd70d20045fe8ccaa9d2
mariosn-phys <132878795+mariosn-phys@users.noreply.github.com>
2024-04-02 14:32:58 +0300

    Update initialize_dist.m

--- DNS_Library_v1/Functions_modal/initialize_dist.m
+++ DNS_Library_v1/Functions_modal/initialize_dist.m
@@ -39,25 +39,27 @@ display('Replacing with previous disturbances')
 [vi,gi,UP,WP,grold,NL,KL,rtold]=read_from_disk_compact_ensemble_opti([field_path,start_mode]);
 [u1,v1,w1,g1]=make_uw_ens_Lyap(vi,gi,UP,WP,0*b1,0*b2,NL,KL);
 
-
-
 %% %init if 1
   Lkiter=0;
-  for Lk=1:min(KL,length(kLyap))
+  for Lk=1:min(length(KL),length(kLyap))
     Lkiter=Lkiter+1;
-      %
+    %
      for Ln=1:min(NL(Lkiter),NLyap(Lkiter))
 
-       uL(:,:,:,Ln,Lk)=u1(:,:,:,Ln,Lk);
-       vL(:,:,:,Ln,Lk)=v1(:,:,:,Ln,Lk);
-       wL(:,:,:,Ln,Lk)=w1(:,:,:,Ln,Lk);
-       gL(:,:,:,Ln,Lk)=g1(:,:,:,Ln,Lk);
+       Lk1=find(KL==kLyap(Lkiter));
+
+       if Lk1, 
+       uL(:,:,:,Ln,Lk)=u1(:,:,:,Ln,Lk1);
+       vL(:,:,:,Ln,Lk)=v1(:,:,:,Ln,Lk1);
+       wL(:,:,:,Ln,Lk)=w1(:,:,:,Ln,Lk1);
+       gL(:,:,:,Ln,Lk)=g1(:,:,:,Ln,Lk1);
+       else
+       end
  %
      end
  %
   end
-
-  
+ 
 end
 
 [uLn,vLn,wLn,gLn,Lkn]=compact_Lyap_Fn(uL,vL,wL,gL);
