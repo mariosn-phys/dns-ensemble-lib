73ff0cad07c1f5f57468190660300771e98aaa36
mariosn-phys <132878795+mariosn-phys@users.noreply.github.com>
2024-05-08 15:51:38 +0300

    Update Base_flow_modal_analysis_v1g.m

--- DNS_Library_v1/Base_flow_modal_analysis_v1g.m
+++ DNS_Library_v1/Base_flow_modal_analysis_v1g.m
@@ -1,3 +1,21 @@
+
+%    Power method eigenanalysis of plane parallel flows (Couette/ Poiseuille) for MATLAB   
+%    Copyright (C) 2024 Marios-Andreas Nikolaidis
+%    Developed during the author's thesis at the University of Athens (NKUA)
+
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU Affero General Public License as published
+%    by the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU Affero General Public License for more details.
+
+%    You should have received a copy of the GNU Affero General Public License
+%    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 clear all;
 
 global M N Re NX D1x D2x D1z D2z DYF D2F D2 D4 y gamma
