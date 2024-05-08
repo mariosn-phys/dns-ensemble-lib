b536a6dface2550d4c3cfc7caecd6b25a6c86e20
mariosn-phys <132878795+mariosn-phys@users.noreply.github.com>
2024-05-08 15:27:11 +0300

    Add files via upload

--- DNS_Library_v1/init_path.m
+++ DNS_Library_v1/init_path.m
@@ -1,9 +1,11 @@
 init_structure = 1; %% Generates the noise basis for a 61x72x72 grid with provided parameters, set to 0 after 1st run
                     
-%% add current folder and subfolders 
+% % add current folder and subfolders 
 %addpath(genpath('../'))
-path_list={'Functions/','Functions_cheb/','Functions_ensemble/','Functions_metrics/','Functions_modal/','Functions_stochastic/'}
-addpath(path_list)
+path_list=["Functions/";"Functions_cheb/";"Functions_ensemble/";"Functions_metrics/";"Functions_modal/";"Functions_stochastic/";"Functions_plot/"];
+for ij=1:length(path_list)
+addpath(path_list(ij))
+end
 
 if init_structure
 R=1000;a=2/2;b=2/1;
