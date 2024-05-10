398028dfe7562106c29d8cb64e1077540b5c42da
mariosn-phys <132878795+mariosn-phys@users.noreply.github.com>
2024-05-10 15:48:21 +0300

    Update init_path.m

--- DNS_Library_v1/init_path.m
+++ DNS_Library_v1/init_path.m
@@ -1,4 +1,4 @@
-init_structure = 1; %% Generates the noise basis for a 61x72x72 grid with provided parameters, set to 0 after 1st run
+init_structure = 0; %% Generates the noise basis for a 61x72x72 grid with provided parameters, set to 0 after 1st run
                     
 % % add current folder and subfolders 
 %addpath(genpath('../'))
@@ -9,6 +9,6 @@ end
 
 if init_structure
 R=1000;a=2/2;b=2/1;
-N=61;NX=72;MZ=72;
-NoiseStructure2_cheb(R,N,NX,MZ,a,b,1);
+N=61;NX=72;MZ=72;sav=1;
+NoiseStructure2_cheb(R,N,NX,MZ,a,b,sav);
 end
